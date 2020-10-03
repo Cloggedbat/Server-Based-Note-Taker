11b2761b70707852b8e42b6badae17419b58a5c2
cleminator <alvinclemens@gmail.com>
2020-10-03 16:47:12 -0600

    still can not link css to the html in the server, all works on live vs code page? i have no idea why

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Router } = require("express");
 // ==============================================================================
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
@@ -18,16 +19,9 @@ var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 
-// turne off the app.use untill i have better understanding
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+app.use(express.static('public'));
 
 //routes to pages 
 
@@ -35,6 +29,9 @@ app.get("/notes", function (req, res){
   res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 })
 
+app.get("/", function (req, res){
+  res.sendFile(path.join(__dirname, "/Develop/public"));
+})
 
 app.get("*", function (req, res){
   res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
@@ -44,12 +41,11 @@ app.get("*", function (req, res){
 // these have been turned off for now 10/2
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
+
+
+// "starts" our server
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);

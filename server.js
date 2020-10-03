9ffe4eb95f89c2221009002f1433f29de1a58884
cleminator <alvinclemens@gmail.com>
2020-10-03 14:56:23 -0600

    the server will go to index.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ==============================================================================
 
 var express = require("express");
-
+const path = require("path")
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -29,7 +29,9 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-
+app.get("/", function (req, res){
+  res.sendFile(path.join(__dirname,"/Develop/public/index.html"));
+})
 // these have been turned off for now 10/2
 
 // require("./routes/apiRoutes")(app);
